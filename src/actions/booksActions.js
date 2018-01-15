ac80b816eb4afaf732ee542782041faeb515f1f8
Arun Kumar <arunkumar7617@gmail.com>
2018-01-15 19:44:33 +0530

    removed commented code

--- src/actions/booksActions.js
+++ src/actions/booksActions.js
@@ -14,20 +14,6 @@ export const getBooks = () => {
       });
   };
 };
-// GET BOOKS
-// export function getBooks(book) {
-//   return function(dispatch) {
-//     axios
-//       .get("/api/books")
-//       .then(function(response) {
-//         dispatch({ type: "GET_BOOKS", payload: response.data });
-//       })
-//       .catch(function(err) {
-//         dispatch({ type: "GET_BOOKS_REJECTED", payload: err });
-//       });
-//   };
-// }
-
 // POST A BOOK
 
 export const postBooks = book => {
@@ -46,22 +32,6 @@ export const postBooks = book => {
   };
 };
 
-// export function postBooks(book) {
-//   return function(dispatch) {
-//     axios
-//       .post("/api/books", book)
-//       .then(function(response) {
-//         dispatch({ type: "POST_BOOK", payload: response.data });
-//       })
-//       .catch(function(err) {
-//         dispatch({
-//           type: "POST_BOOK_REJECTED",
-//           payload: "There was an error while posting a new book"
-//         });
-//       });
-//   };
-// }
-
 // DELETE A BOOK
 
 export const deleteBooks = id => {
@@ -76,19 +46,6 @@ export const deleteBooks = id => {
       });
   };
 };
-
-// export function deleteBooks(id) {
-//   return function(dispatch) {
-//     axios
-//       .delete("/api/books/" + id)
-//       .then(function(response) {
-//         dispatch({ type: "DELETE_BOOK", payload: id });
-//       })
-//       .catch(function(err) {
-//         dispatch({ type: "DELETE_BOOK_REJECTED", payload: err });
-//       });
-//   };
-// }
 
 // RESET BUTTON
 export function resetButton() {
